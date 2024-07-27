409ee63370974e94c894bfebab8d3492ca99711e
shreyas <shreyas@shreyass-MacBook-Pro.local>
2024-07-28 03:42:41 +0530

    Modify setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,11 @@
 # Update package list and install poppler-utils
 sudo apt-get update
 sudo apt-get install -y poppler-utils
+
+# Verify Poppler installation
+if command -v pdfinfo &> /dev/null
+then
+    echo "Poppler is installed and in the PATH"
+else
+    echo "Poppler is not in the PATH"
+fi
